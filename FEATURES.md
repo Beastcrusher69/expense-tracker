7d1ca7bd2f2a14be43790312680da98c76300f22
Jay <jaykapadia389@gmail.com>
2023-06-07 21:01:09 +0530

    enter button works on keydown and delete button works with one edge case left

---
+++ FEATURES.md
@@ -0,0 +1,6 @@
+- store data in database
+- authentication
+- upload photos of bill for each expense
+- store expenses date vise
+- display total expense
+- host website
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
 import { useState , useEffect} from 'react'
 import './App.css'
 
-let tableRows=[]
-let initArr = ['jay','kapadia',tableRows];
-let keyVar = 1;
-
-
 function Display(props){
 let {data}=props;
   return ( 
@@ -16,7 +11,7 @@ let {data}=props;
       </thead>
       <tbody>
         {data.map((d)=>{
-          return <div>{d.purpose}</div>
+          return <tr><td>{d.purpose}</td><td>{d.expense}</td><td><button className="delete" onClick={()=>{props.handleDelete(d)}}>Delete</button></td></tr>
         })}
       </tbody>
     </table>
@@ -30,51 +25,37 @@ let [data , setData] =  useState([]);
 let [input , setInput] = useState({purposeValue:null,expenseValue:null})
 //[{purpose:'jay',expense:'200'}]
 
+    useEffect(()=>{
+    window.addEventListener('keydown',(e)=>{
+      if(e.code === "Enter"){
+        handleSubmit();
+      }
+    })
+  })
+
   function handleSubmit(e){
-
     console.log(input);
-
-
     if(input.purposeValue && input.expenseValue){
     setData([...data,{purpose:input.purposeValue,expense:input.expenseValue}])
     }
   }
 
-  function handleKeyDown(e){
-
-    console.log(e.code);
-
-    if(e.code == "Enter"){
-            handleSubmit();
-    }
+  function handleDelete(deleteData){
+    let filteredData = data.filter((d)=>(d.expense != deleteData.expense || d.purpose != deleteData.purpose));
+    setData(filteredData);
   }
 
-  function deleteRow(e){
-
-    let id = e.target.id ;   
-
-    let newArroftr = Arr[2].filter((elem)=>{
-      
-      return (elem.key != id); 
-    });
-   
-    setArr([Arr[0],Arr[1],newArroftr]);
-  }
 
   return(
     <div id="whole-wrapper">
     <div id="input-wrap">
-      <input  onChange={(e)=>{ e.preventDefault();console.log(e.target.value) ;setInput({...input , purposeValue:e.target.value})}} name="purpose" placeholder='purpose' onKeyDown={handleKeyDown}/>
-      <input  onChange={(e)=>{ e.preventDefault(); console.log(e.target.value) ;setInput({...input ,expenseValue:e.target.value})}} name="expense" placeholder='expense' onKeyDown={handleKeyDown}/>
-
+      <input  onChange={(e)=>{ e.preventDefault() ;setInput({...input , purposeValue:e.target.value})}} name="purpose" placeholder='purpose' />
+      <input  onChange={(e)=>{ e.preventDefault() ;setInput({...input ,expenseValue:e.target.value})}} name="expense" placeholder='expense' />
       <button id="input-button" onClick={handleSubmit} >Enter</button>
     </div>
-    <Display data={data}/>
+    <Display data={data} handleDelete={handleDelete}/>
     </div>
-   
   )
-
 }
-
 
 export default App
